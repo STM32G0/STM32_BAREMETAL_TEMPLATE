1275381af812bbd8cc85839489c737275dc340e1
STM32G0 <stm32g0@gmail.com>
2022-12-13 18:47:57 +0100

    init

---
+++ STM32G0_BareMetalSzablon/Src/main.c
@@ -0,0 +1,25 @@
+/*****************************************
+File  : main.c
+Autor : strefapic.blogspot.com
+MCU   : STM32G071KBT6
+IDE   : STM32Cube IDE
+REV   : x.x
+******************************************/
+
+#include <stm32g071xx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "stm32g0xx_it.h" // interrupt handlers
+
+
+
+
+int main(void) {
+
+
+	while(1){
+
+
+	}
+}
